20831b5dc806616e919951ef91a875cc565ea5c0
lina noirot <lina.noirot@gmail.com>
2022-12-01 10:48:58 +0100

    backend auth with jwt complete without link to the db

---
+++ nest/backend/src/auth/constants.ts
@@ -0,0 +1,11 @@
+export const jwtConstants = {
+	secret: 'secretKey',
+  };
+
+  /***********************
+   * 
+   * 
+   * Need to protect correctly this value !
+   * Use PEM-encoded public key ?
+   * 
+   */
