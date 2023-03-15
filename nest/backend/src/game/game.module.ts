cc4d4866c31053ff7d788c9ec719fed8fc9ec9c2
lina noirot <lina.noirot@gmail.com>
2023-03-15 17:23:25 +0100

    never listen to other people

--- nest/backend/src/game/game.module.ts
+++ nest/backend/src/game/game.module.ts
@@ -6,11 +6,14 @@ import { Game } from '../database/entities/Game';
 import { GameGateway } from './game.gateway';
 import { UserService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { AuthService } from 'src/auth/auth.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Game]), UsersModule],
+    imports: [TypeOrmModule.forFeature([Game]), UsersModule, AuthModule],
     controllers: [GameController],
-    providers: [ GameGateway, GameService, Logger],
+    providers: [ GameGateway, GameService, Logger, JwtService],
     exports: [Logger]
 })
 export class GameModule { }
