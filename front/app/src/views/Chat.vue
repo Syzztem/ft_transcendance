f85abddec5071bbad33c95ae059a82533d60f62f
lothieve <lothieve@student.42.fr>
2023-04-01 11:37:27 +0200

    added error handlingin front

--- front/app/src/views/Chat.vue
+++ front/app/src/views/Chat.vue
@@ -208,9 +208,6 @@ export default defineComponent({
 
 		})
 		chatSocket.on('left_channel' , (res: any) => {
-			console.log("wubba lubba dub dub");
-			console.log('leave channel uid dto :' , res.uid);
-			console.log(`User ${res.uid} left channel ${res.channel}`);
 			if (res.uid == this.id)
 				this.rmChannel(res.channel.id);
 			else
@@ -228,6 +225,9 @@ export default defineComponent({
 		chatSocket.on('deleteChannel', (res: IChannel) => {
 			console.log(res.name, " was deleted");
 			this.rmChannel(res.id);
+		})
+		chatSocket.on('error', args => {
+			console.log(args);
 		})
 	},
 	unmounted() {
